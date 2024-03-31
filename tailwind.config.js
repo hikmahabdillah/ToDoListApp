d34437ce2edcdfe6f5dd11281423d0824b00a3cf
Hikmah Aldrin Abdillah <hikmahaldrin44@gmail.com>
2024-03-31 20:01:14 +0700

    initial commit

---
+++ tailwind.config.js
@@ -0,0 +1,12 @@
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+  content: ["./public/**/*.{html,js}"],
+  theme: {
+    fontFamily: {
+      inter: ["Inter", "sans-serif"],
+      poppins: ["Poppins", "sans-serif"],
+    },
+    extend: {},
+  },
+  plugins: [],
+};
