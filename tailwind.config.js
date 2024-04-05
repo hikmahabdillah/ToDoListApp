e9a5f2bb3c4c4fa1b7cccab59f32274bc769b5b5
Hikmah Aldrin Abdillah <hikmahaldrin44@gmail.com>
2024-04-05 23:12:44 +0700

    add header and nav of body

--- tailwind.config.js
+++ tailwind.config.js
@@ -6,7 +6,19 @@ module.exports = {
       inter: ["Inter", "sans-serif"],
       poppins: ["Poppins", "sans-serif"],
     },
-    extend: {},
+    extend: {
+      animation: { updown: "upndown 5s linear infinite" },
+      keyframes: {
+        upndown: {
+          "0%, 100%": {
+            transform: "translateY(0px) scale(1.1)",
+          },
+          "50%": {
+            transform: "translateY(-30px) scale(1.1)",
+          },
+        },
+      },
+    },
   },
   plugins: [],
 };
