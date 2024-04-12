c9de5bc04c633960a13a4d7d7513d66d8588a193
Hikmah Aldrin Abdillah <hikmahaldrin44@gmail.com>
2024-04-12 22:29:14 +0700

    add task detail section, add event on add task & detail section, and formatting code

--- public/js/script.js
+++ public/js/script.js
@@ -2,8 +2,15 @@ document.addEventListener("DOMContentLoaded", () => {
   const btnContinue = document.querySelector(".btn-continue");
   const overlay = document.querySelector(".overlay");
   const back = document.querySelector(".back");
+  const backedit = document.querySelector(".back-edit");
+  const edit = document.querySelector(".edit");
   const btnAdd = document.querySelector(".btn-add");
   const bgcontent = document.querySelector(".bg-overlay-content");
+  const bgdetail = document.querySelector(".bg-overlay-detail");
+  const backdetail = document.querySelector(".back-detail");
+  const card = document.querySelector(".card");
+  const bgeditcontent = document.querySelector(".bg-overlay-content-edit");
+  const dropdownHorizon = document.querySelector("#dropdownDotsHorizontal");
 
   btnContinue.addEventListener("click", () => {
     overlay.classList.add("disp");
@@ -16,14 +23,46 @@ document.addEventListener("DOMContentLoaded", () => {
     bgcontent.classList.remove("disp");
     bgcontent.style.display = "block";
     setTimeout(function () {
-      bgcontent.style.transform = "translateX(0)";
+      bgcontent.style.transform = "translateY(0)";
     }, 200);
   });
 
   back.addEventListener("click", () => {
-    bgcontent.style.transform = "translateX(100%)";
+    bgcontent.style.transform = "translateY(-100%)";
     setTimeout(function () {
       bgcontent.style.display = "none";
     }, 400);
   });
+
+  card.addEventListener("click", () => {
+    bgdetail.classList.remove("disp");
+    bgdetail.style.display = "block";
+    setTimeout(function () {
+      bgdetail.style.transform = "translateY(0)";
+    }, 200);
+  });
+
+  backdetail.addEventListener("click", () => {
+    bgdetail.style.transform = "translateY(100%)";
+    setTimeout(function () {
+      bgdetail.style.display = "none";
+    }, 400);
+  });
+
+  // edit.addEventListener("click", () => {
+  //   dropdownHorizon.style.display = "none";
+  //   bgeditcontent.classList.remove("disp");
+  //   bgeditcontent.style.display = "block";
+  //   setTimeout(function () {
+  //     bgeditcontent.style.transform = "translateX(0)";
+  //   }, 200);
+  // });
+
+  // backedit.addEventListener("click", () => {
+  //   bgeditcontent.style.transform = "translateX(100%)";
+  //   setTimeout(function () {
+  //     dropdownHorizon.style.display = "block";
+  //     bgeditcontent.style.display = "none";
+  //   }, 400);
+  // });
 });
