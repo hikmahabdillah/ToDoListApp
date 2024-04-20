f4f86c8b9f83b49c7b2bda98d334b5d664f0779a
Hikmah Aldrin Abdillah <hikmahaldrin44@gmail.com>
2024-04-20 13:55:59 +0700

    add function addTask with local storage and add function delete All Task

--- public/js/script.js
+++ public/js/script.js
@@ -15,62 +15,113 @@ document.addEventListener("DOMContentLoaded", () => {
   const formAdd = document.getElementById("addTask");
   const formUpdate = document.getElementById("updateTask");
   const tabs = document.querySelectorAll(".tab-option");
-
-  // FORM EVENT
   const cardPlace = document.querySelector(".card-place");
 
-  // ADD TASK FEATURE
-  const addTask = () => {
-    let taskName = document.querySelector('input[name="task-name"]');
-    let startTime = document.querySelector('input[name="startTime"]');
-    let endTime = document.querySelector('input[name="endTime"]');
-    let descriptionTask = document.querySelector(".description");
+  // LOAD TASK FROM LOCAL STORAGE
+  function loadTasks() {
+    const tasks = localStorage.getItem("TODOOAPPS");
+    return tasks ? JSON.parse(tasks) : {};
+  }
 
-    // selection time
-    let taskTime;
-    if (
-      startTime.value.split(":")[0] != 0 &&
-      endTime.value.split(":")[0] != 0
-    ) {
-      taskTime = `
-      <span class="time-badge absolute bottom-0 left-0">
-        ${startTime.value} ${startTime.value.split(":")[0] < 12 ? "am" : "pm"} -
-        ${endTime.value} ${endTime.value.split(":")[0] < 12 ? "am" : "pm"}
-      </span>
-      `;
-    } else {
-      taskTime = "";
-    }
-
-    // ADD CARD
-    const newCard = `<div class="card relative btn-hover hover-95">
-    <div class="headCard col-span-2 flex justify-between items-center h-7">
-      <span class="status bg-yellow-900 text-yellow-300 text-sm font-medium me-2 px-2.5 py-0.5 rounded-md">Incompleted</span>
-      <div class="flex gap-1 p-3 [&>img]:rounded-lg [&>img]:p-1">
-        <img src="./img/Edit2.png" class="edit hover:scale-90 hover:ring-orange-400 hover:ring-2 transition-all duration-300" width="35px" alt="">
-        <img src="./img/Delete2.png" class="hover:scale-90 hover:ring-orange-400 hover:ring-2 transition-all duration-300" data-modal-target="popup-modal" data-modal-toggle="popup-modal" width="35px"
-          alt="">
-      </div>
-    </div>
-    <h2 class="font-semibold text-[#562D00] text-xl col-span-2 max-w-[80%] mt-2">
-      ${taskName.value}
-    </h2>
-    <p class="text-gray-600 text-sm col-span-2 line-clamp-2 text-balance text-ellipsis overflow-hidden max-w-[95%] mb-4">${descriptionTask.value}</p>
-    <div class="flex items-center me-4 col-span-2 justify-self-end">
-      <input id="orange-checkbox" type="checkbox" value="" class="w-6 h-6 text-[#FF8400] bg-gray-100 border-gray-300 rounded focus:ring-[#FF8400] dark:focus:ring-[#ff8400ad] focus:ring-2 ">
-      <label for="orange-checkbox" class="ms-2 text-sm font-medium text-[#FF8400] dark:text-gray-300">Done</label>
-  </div>
-  ${taskTime}
-  </div>`;
-    cardPlace.insertAdjacentHTML("beforeend", newCard);
-  };
-
-  document.querySelector("form#addTask").addEventListener("submit", () => {
-    addTask();
+  document.querySelector(".deleteAll").addEventListener("click", () => {
+    localStorage.removeItem("TODOOAPPS");
+    cardPlace.innerHTML = "";
+    // deleteLocal();
   });
+  // function deleteLocal() {
+  // }
 
-  // REMOVE TASK
-  const removeTask = () => {};
+  // GET TASK
+  function getTask() {
+    return loadTasks();
+  }
+
+  // SAVE TASK ON LOCAL STORAGE
+  function saveTask(tasks) {
+    localStorage.setItem("TODOOAPPS", JSON.stringify(tasks));
+  }
+
+  // CREATE TASK
+  function createTask(
+    id,
+    taskName,
+    description,
+    startTime,
+    endTime,
+    isComplete
+  ) {
+    const tasks = loadTasks();
+    tasks[id] = { taskName, description, startTime, endTime, isComplete };
+    saveTask(tasks);
+  }
+
+  // GET ID
+  function getTaskById(id) {
+    const tasks = loadTasks();
+    return tasks[id];
+  }
+
+  // ADD TASK FEATURE
+  function addTask() {
+    let taskName = document.querySelector('input[name="task-name"]').value;
+    let startTime = document.querySelector('input[name="startTime"]').value;
+    let endTime = document.querySelector('input[name="endTime"]').value;
+    let descriptionTask = document.querySelector(".description").value;
+    const id = Date.now().toString();
+    createTask(id, taskName, descriptionTask, startTime, endTime, false);
+    displayTask();
+  }
+
+  const displayTask = function () {
+    const tasks = getTask();
+
+    // Delete all existing tasks before displaying new ones to avoid the duplicated task on cardplace section
+    cardPlace.innerHTML = "";
+
+    for (const id in tasks) {
+      const task = tasks[id];
+
+      // selection time
+      let taskTime;
+      if (
+        task.startTime.split(":")[0] != 0 &&
+        task.endTime.split(":")[0] != 0
+      ) {
+        taskTime = `
+        <span class="time-badge absolute bottom-0 left-0">
+          ${task.startTime} ${task.startTime.split(":")[0] < 12 ? "am" : "pm"} -
+          ${task.endTime} ${task.endTime.split(":")[0] < 12 ? "am" : "pm"}
+        </span>
+        `;
+      } else {
+        taskTime = "";
+      }
+
+      // ADD CARD
+      const newCard = document.createElement("div");
+      newCard.classList.add("card", "relative", "btn-hover", "hover-95");
+
+      newCard.innerHTML = `
+        <div class="headCard col-span-2 flex justify-between items-center h-7">
+          <span class="status bg-yellow-900 text-yellow-300 text-sm font-medium me-2 px-2.5 py-0.5 rounded-md">Incompleted</span>
+          <div class="flex gap-1 p-3 [&>img]:rounded-lg [&>img]:p-1">
+            <img src="./img/Edit2.png" class="edit hover:scale-90 hover:ring-orange-400 hover:ring-2 transition-all duration-300" width="35px" alt="">
+            <img src="./img/Delete2.png" class="hover:scale-90 hover:ring-orange-400 hover:ring-2 transition-all duration-300" data-modal-target="popup-modal" data-modal-toggle="popup-modal" width="35px" alt="">
+          </div>
+        </div>
+        <h2 class="font-semibold text-[#562D00] text-xl col-span-2 max-w-[80%] mt-2">${task.taskName}</h2>
+        <p class="text-gray-600 text-sm col-span-2 line-clamp-2 text-ellipsis max-w-[95%] mb-4">${task.description}</p>
+        <div class="flex items-center me-4 col-span-2 justify-self-end">
+          <input id="orange-checkbox" type="checkbox" value="" class="w-6 h-6 text-[#FF8400] bg-gray-100 border-gray-300 rounded focus:ring-[#FF8400] dark:focus:ring-[#ff8400ad] focus:ring-2 ">
+          <label for="orange-checkbox" class="ms-2 text-sm font-medium text-[#FF8400] dark:text-gray-300">Done</label>
+        </div>
+          ${taskTime}
+      `;
+
+      // APPEND NEW ELEMENT TO PARENT
+      cardPlace.appendChild(newCard);
+    }
+  };
 
   btnContinue.addEventListener("click", () => {
     overlay.classList.add("disp");
@@ -146,6 +197,7 @@ document.addEventListener("DOMContentLoaded", () => {
   // });
 
   formAdd.addEventListener("submit", (event) => {
+    addTask();
     event.preventDefault();
     bgcontent.style.transform = "translateY(-100%)";
     setTimeout(function () {
@@ -185,4 +237,8 @@ document.addEventListener("DOMContentLoaded", () => {
       this.classList.add("active"); // Tambahkan kelas aktif ke tab yang diklik
     });
   });
+
+  window.onload = function () {
+    displayTask();
+  };
 });
